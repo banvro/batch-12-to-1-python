e2594662bbc999dba0eaa551924f23eef92a0f5f
Naresh Kumar <bnavro07@gmail.com>
2023-08-03 12:56:01 +0530

    #3rd class

---
+++ 3rdclass.py
@@ -0,0 +1,108 @@
+# python history
+
+
+# ABC Language: => Guido van rossum
+
+
+# ABC langauge
+# modulo-3
+# c
+# unix
+# ...
+# ...
+
+# 1980s ---------- 1991   [python]
+
+# python 0.9.0
+
+
+# 2000  ::: python 2
+
+
+# 2020
+
+# 2008 ::::  python 3
+
+
+
+# diffrence between python 2 or python 3
+
+
+# print "helo wold"
+
+
+# print("hello word")
+
+
+
+# range()
+
+# xrange()
+
+
+# identifires
+
+# abc = 20
+
+# keywords : 
+
+a = 20
+
+# int float str list tuple len 
+
+
+int = 20
+
+
+
+# int main(){
+#     wkjfbkwekwk
+#     weljrbwejrbkwer
+#     \wejrbwekrbwkebrjkw
+# }
+
+
+
+# if 10 == 10:
+#     print("ok")
+#     print("hello")
+
+# print("sabdad")
+
+
+# print()
+
+
+# +, -, /, *, %, **
+
+
+
+# 10 + 20
+
+
+# sdbfdbfjsdbfk
+
+"""
+askdjbakjsdbkajsbdaksdjka
+"""
+
+
+# help(print)
+
+
+def abc():
+    """
+    how to use
+    kskasd
+    asajbdjabsdkabsdkjasbdka
+    """
+    print("i am a devloper..")
+    
+    
+help(abc)
+
+
+
+
+
+
