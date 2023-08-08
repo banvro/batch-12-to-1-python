192e4970d8dcebadc4be13a5611372a13b400bb4
Naresh Kumar <bnavro07@gmail.com>
2023-08-08 12:57:42 +0530

    File handeling

---
+++ 6thclass.py
@@ -0,0 +1,45 @@
+# file handeling : 
+# a = 10
+# b = 20
+
+# c = a + b
+
+# print(c)  
+
+
+
+# file = open('mydol/hello.txt', 'r')
+
+# print(file.read())
+# print(file.readline(12))
+# print(file.readline())
+
+
+# to write in file 
+
+# file = open('mydol/helloxyz.txt', 'w')
+
+# file.write("xyz abc nw data")
+
+# file = open('mydol/helloabc.txt', 'a')
+
+# file.write(" \n this data come form apend \n")
+
+
+
+# a = 10
+# b = 20
+# c = a + b
+
+# d = str(c)
+
+# fi = open("mydata.txt", 'w')
+
+# fi.write(f"the sum of var a and var b : {c}")
+
+
+
+
+
+
+
