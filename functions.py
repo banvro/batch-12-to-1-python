7a8609b795d801c16f7e249e0f7e73c73c224a87
Naresh Kumar <banvro07@gmail.com>
2023-08-09 14:16:34 +0530

    Strat functions

---
+++ functions.py
@@ -0,0 +1,70 @@
+# functions:  code optimization
+    # user define
+    # built in function
+    
+# a = 10
+# b = 20
+# c = a + b
+# print(c)
+
+# decleartion of function
+# def xyz():
+#     print("helllo")
+#     print("this is me")
+    
+
+# # call a function
+
+# xyz()
+
+# xyz()
+
+
+# xyz()
+
+# xyz()
+
+# perameters
+# arguments
+
+
+# def summ(a, b):
+#     sum = a + b
+#     print(sum)
+ 
+
+# # arguments   
+# summ(12, 10)
+
+# summ(10, 20)
+
+# num1 = int(input("Enter first value : "))
+# num2 = int(input("Enter second value : "))
+
+# summ(num1, num2)
+
+
+# def .....
+
+
+
+
+# ..(3)
+
+# 3 x 1 = 3
+# 3 x 2 = 6
+# 3 x 3 = 9
+# ...
+
+
+# ...
+
+
+def dcf(a = 10, b = 20):
+    c = a + b
+    print(c)
+    
+dcf(12, 2)
+
+
+
