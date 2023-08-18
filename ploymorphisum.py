d0a81327101fd1d35887db740d6726874d5d0f79
Naresh Kumar <banvro07@gmail.com>
2023-08-18 13:11:15 +0530

    revsion while or for

---
+++ ploymorphisum.py
@@ -0,0 +1,74 @@
+# ploymorphisum : 
+
+# ploy :    Many
+# morphisum :    forms
+    # 1) Duck typing
+    # 2) Operator overloading
+    # 3) Method overloading
+    # 4) Mehtod overriding
+
+
+
+# functions :
+
+# while 
+
+
+
+# while True:
+#     inp = input("press q for quit and s for start.. : ")
+
+#     if inp == "q":
+#         break
+
+#     elif inp == "s":
+#         ftrl = int(input("Enter you want to make factorial : ")) 
+#         oupt = 1
+
+#         for i in range(1, ftrl + 1):
+#             oupt = oupt * i
+        
+#         print("The factoril is : ", oupt)
+
+
+#     else:
+#         print("Enter something vaild")
+#         break
+
+
+# for i in range(5):
+#     print(i)
+
+
+
+# a = [1, 3, 5, 7, 8, 10]
+
+# print(list(reversed(a)))
+# print(a)
+
+
+
+# print(sum(a))
+
+
+# for i in range(5, 0, -1):
+#     print(i)
+
+
+
+# method overloading : 
+
+
+# method overriding
+
+
+
+
+
+# a = 10
+
+# a = 100
+
+
+
+
