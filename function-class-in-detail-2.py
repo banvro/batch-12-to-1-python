0b1bad17e8348f3a91e27b1148791508685d7e3c
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2023-08-21 21:38:45 -0700

    Create function-class-in-detail-2.py

---
+++ function-class-in-detail-2.py
@@ -0,0 +1,45 @@
+    # lambda function
+
+    # lambda arguments : expression 
+
+    # zx = lambda a : a + 2
+
+    # print(zx(12))
+
+    # map()
+
+    # map(function, intrater)
+
+    # a = [1, 2, 3, 4, 6, 7, 10]
+
+
+    # zx = map(lambda x : x + 2,  a)
+
+    # za = list(zx)
+
+    # print(za)
+
+    # filter()
+
+    # filter(funcation, intrater)
+
+    # a = [2, 3, 1, 4, 5, 7, 10, 21, 23, 26]
+
+    # zx = filter(lambda x : x // 2 == 0, a)
+
+
+    # 2.6
+
+    # print(list(zx))
+
+    # reduce()  => 
+
+    # functools
+
+    # from functools import reduce
+
+    # a = [1, 2, 3, 4, 6, 1]
+
+    # zx = reduce(lambda x, y : x + y, a)
+
+    # print(zx)
