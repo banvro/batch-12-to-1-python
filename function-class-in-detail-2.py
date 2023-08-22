55a688f39ae49155fce4ba8b9897da3b2d2b25b1
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2023-08-21 22:24:55 -0700

    Update function-class-in-detail-2.py

--- function-class-in-detail-2.py
+++ function-class-in-detail-2.py
@@ -1,45 +1,111 @@
-    # lambda function
+#     # lambda function
 
-    # lambda arguments : expression 
+#     # lambda arguments : expression 
 
-    # zx = lambda a : a + 2
+#     # zx = lambda a : a + 2
 
-    # print(zx(12))
+#     # print(zx(12))
 
-    # map()
+#     # map()
 
-    # map(function, intrater)
+#     # map(function, intrater)
 
-    # a = [1, 2, 3, 4, 6, 7, 10]
-
-
-    # zx = map(lambda x : x + 2,  a)
-
-    # za = list(zx)
-
-    # print(za)
-
-    # filter()
-
-    # filter(funcation, intrater)
-
-    # a = [2, 3, 1, 4, 5, 7, 10, 21, 23, 26]
-
-    # zx = filter(lambda x : x // 2 == 0, a)
+#     # a = [1, 2, 3, 4, 6, 7, 10]
 
 
-    # 2.6
+#     # zx = map(lambda x : x + 2,  a)
 
-    # print(list(zx))
+#     # za = list(zx)
 
-    # reduce()  => 
+#     # print(za)
 
-    # functools
+#     # filter()
 
-    # from functools import reduce
+#     # filter(funcation, intrater)
 
-    # a = [1, 2, 3, 4, 6, 1]
+#     # a = [2, 3, 1, 4, 5, 7, 10, 21, 23, 26]
 
-    # zx = reduce(lambda x, y : x + y, a)
+#     # zx = filter(lambda x : x // 2 == 0, a)
 
-    # print(zx)
+
+#     # 2.6
+
+#     # print(list(zx))
+
+#     # reduce()  => 
+
+#     # functools
+
+#     # from functools import reduce
+
+#     # a = [1, 2, 3, 4, 6, 1]
+
+#     # zx = reduce(lambda x, y : x + y, a)
+
+#     # print(zx)
+
+
+
+
+
+
+
+# a = [23, 100, 46, 90, 30, 45, 32, 98]
+# print(a)
+
+# # +  10
+
+# za = map(lambda x : x + 10, a)
+# # print(list(za))
+# aw = list(za)
+# # /2
+
+# print(aw, 'iiiiiiii')
+
+# xs = filter(lambda k : k % 2 == 0, aw)
+# # print(list(xs))
+# # for i in xs:
+# #     print(i)
+# io = list(xs)
+# print(io)
+# # ++
+# print(io, "yyyyyyyyyyyyyyyyyyyy")
+
+# from functools import reduce
+
+# yu = reduce(lambda x, y : x + y, io)
+
+# print(yu)
+
+
+
+
+
+
+# function dacorator 
+
+# function dacoratoers
+
+def decot(fun):
+    def waper(*args, **kwargs):
+        print("weclome to my function ...this helps you alott...lets go")
+        
+        fun(*args, **kwargs)
+        
+        print("thank you for using me...")
+        
+    return waper
+
+@decot
+def syi(a, b):
+    c = a + b
+    print("output = ", c)
+    
+# syi(100, 200)
+
+
+@decot
+def ui():
+    print("nooo")
+    
+ui()
