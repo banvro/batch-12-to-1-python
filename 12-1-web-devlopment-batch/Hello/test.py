bb16efe8c73a8e088ed88ec1258a5dd55d505ea3
Naresh Kumar <banvro07@gmail.com>
2023-08-24 00:37:37 -0700

    Start Django batchhes

---
+++ 12-1-web-devlopment-batch/Hello/test.py
@@ -0,0 +1,18 @@
+# Djanngo : 
+# asgi.py
+
+
+# to create project
+#     django-admin startproject Hello
+#     or 
+#     python -m django startproject Hello
+
+# to start project:
+#     pythom manage.py runserver
+
+# to apply migrations to database
+    # python manage.py migrate
+
+# createsuperuser
+    # python manage.py createsuperuser
+
