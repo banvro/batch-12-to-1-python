e34c74bab5ae60015a746dc3787ec323e645b842
Naresh Kumar <banvro07@gmail.com>
2023-08-25 00:34:04 -0700

    set urls

--- 12-1-web-devlopment-batch/Hello/Hello/asgi.py
+++ 12-1-web-devlopment-batch/Hello/Hello/asgi.py
@@ -14,3 +14,7 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Hello.settings')
 
 application = get_asgi_application()
+
+
+
+# sockets
---
+++ 12-1-web-devlopment-batch/Hello/hlo/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path, include
+from hlo import views
+
+urlpatterns = [
+    path('car', views.car)
+]
