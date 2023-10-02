082f00ee7e721fb8486553c939a229db93867f86
Naresh Kumar <banvro07@gmail.com>
2023-10-02 00:51:31 -0700

    loops

---
+++ october-batch/python-loops.py
@@ -0,0 +1,147 @@
+# Loops : are used for ittration
+
+
+# a = "hlo this is me"
+
+# for i in a:
+#     print(i)
+#     print("helloooo")
+#     print("xyzzzzzzz")
+
+# a = 10000
+
+# for z in a:
+#     print(z)
+
+
+
+# range(start, end, inccreament)
+
+# strat = 0
+# end = n -1
+# incremnet = 1
+
+# range(2, 10, 2)
+
+
+# for i in range(2, 10, 3):
+#     print(i)
+
+
+# lst = ["heo", "er", "ty"]
+
+# for i in lst:
+#     print(i)
+
+# print()
+
+
+
+# 19 x 2 = 19
+# 19 x 3 = 38
+# 19 x 4 = 57
+# .
+# .
+# .
+# .
+
+# 19 x 11 = 190
+
+# for i in range(1, 11):
+#     print("19 x ", i, "=", 19 * i)
+
+# lst = [10, 30, 34, 100, 78, 4, 6, 12, 1]
+
+# zx = 0
+
+# for i in lst:
+#     zx = zx + i  # 10, 40
+
+# print(zx)
+
+
+# 2) While Loop : 
+
+
+# age = 10
+
+# # 100 > 100
+# while age < 100:
+#     print("hello...", age)
+#     age = age + 10
+
+
+
+# a = 1
+
+# while a <= 10:
+#     print(a)
+#     a = a + 1
+
+
+
+
+
+# 19 x 2 = 19
+# 19 x 3 = 38
+# 19 x 4 = 57
+# .
+# .
+# .
+# .
+
+# 19 x 11 = 190
+
+# a = 0
+
+
+# while a <= 9:
+#     a = a + 1
+#     print("19 x ", a + 1, "=", 19 * a)
+
+
+# Type casting : 
+
+# a = "10"
+# b = 20
+
+# c = a + b
+
+# print(c)
+
+
+
+# a = 10
+
+# print(a, type(a))
+
+# b = float(a)
+# print(b, type(b))
+
+
+# c = str(a)
+# print(c, type(c))
+
+
+
+# a = "12"
+
+# b = int(a)
+
+# zx = 23462374
+
+# for i in zx:
+#     print(i)
+
+
+
+
+qw = 23456123456
+
+
+
+
+
+
+
+
