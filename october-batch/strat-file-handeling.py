5d336209aad9ec97db868bff8ad51026db09912e
Naresh Kumar <banvro07@gmail.com>
2023-10-06 00:10:12 -0700

    exception handeling

---
+++ october-batch/strat-file-handeling.py
@@ -0,0 +1,49 @@
+
+# a = 10
+# b = 200
+
+# c = a + b
+
+# print(c)
+
+
+
+# fil = open("mytxt.txt", "r")
+# print(fil.read())
+# fil.close()
+
+# to write in a text file 
+
+
+# x = open("mytxtabc.txt", "w")
+# x.write("this is new data...")
+# x.close()
+
+
+# x = open("mytxtabc.txt", "a")
+# x.write("\nkkkkkkkkk")
+# x.close()
+
+
+a = 10000
+b = 40000
+
+c = a + b
+# c = str(c)
+
+j = open("result.txt", "w")
+j.write(f"the sum is: {c}")
+j.close()
+
+
+
+
+
+
+
+
+
+
+
+
+
