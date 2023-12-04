9a864d93abdb7d140a0d1217202162716f720ebe
Naresh Kumar <banvro07@gmail.com>
2023-12-03 23:02:40 -0800

    tkinter

--- october-batch/tk-using-grid.py
+++ october-batch/tk-using-grid.py
@@ -32,6 +32,34 @@
 
 
 import tkinter as tk
+import mysql.connector
+
+# con = mysql.connector.connect(host="localhost", username = "root", pssword = "1234", database = "yourdatabasename")
+# cursr = con.cursor()
+
+# cursr.execute("show database Like %s", ("Studnets",))
+# zx = cursr.fetchone()
+
+# if not zx:
+#     cursr.execute("Create database Students")
+
+def savedata():
+    name = ent1.get()
+    age = ent2.get()
+    emial = ent3.get()
+    numbr = ent4.get()
+
+    # cursr.execute(f"insert ito tblname values('{name}', {age},' {emial}', '{numbr}')")
+    # con.commit()
+
+    ent1.delete(0, tk.END)
+    ent2.delete(0, tk.END)
+    ent3.delete(0, tk.END)
+    ent4.delete(0, tk.END)
+
+    mymsg.config(text = "data saved sv=ucsessfullyy" )
+    
+
 
 window = tk.Tk()
 window.geometry("600x400")
@@ -50,12 +78,14 @@ lbl7 = tk.Label(window, text = ":", font = ("bobort", 20, "bold"), bg = "yellow"
 lbl8 = tk.Label(window, text = ":", font = ("bobort", 20, "bold"), bg = "yellow")
 lbl9 = tk.Label(window, text = ":", font = ("bobort", 20, "bold"), bg = "yellow")
 
+mymsg = tk.Label(window, text = "  ", font = ("bobort", 20, "bold"), bg = "yellow", fg="Green")
+
 ent1 = tk.Entry(window, font = ("robort", 20, "italic"))
 ent2 = tk.Entry(window, font = ("robort", 20, "italic"))
 ent3 = tk.Entry(window, font = ("robort", 20, "italic"))
 ent4 = tk.Entry(window, font = ("robort", 20, "italic"))
 
-btn = tk.Button(window, text="Submit", font = ("robort", 20, "bold"))
+btn = tk.Button(window, text="Submit", font = ("robort", 20, "bold"), command = savedata)
 
 
 lbl1.grid(row=0, column=0, padx=25)
@@ -76,6 +106,6 @@ ent4.grid(row=4, column=3)
 
 btn.grid(row=5, column=3)
 
-
+mymsg.grid(row=6, column=3)
 
 window.mainloop()
