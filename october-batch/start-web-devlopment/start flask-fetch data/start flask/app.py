f8e95b566109797b3a6ef2186bd0b35e4a003744
Naresh Kumar <banvro07@gmail.com>
2024-01-09 23:07:54 -0800

    update o delete

--- october-batch/start-web-devlopment/start flask-fetch data/start flask/app.py
+++ october-batch/start-web-devlopment/start flask-fetch data/start flask/app.py
@@ -37,6 +37,32 @@ def showingthis():
     # print(alldata, "all data")
     return render_template("showthisdata.html", allrecords = alldata)
 
+@myapp.route("/deletethis/<xyz>", methods = ["POST"])
+def deletdata(xyz):
+    cuser.execute(f"delete from savemydataa where Name = '{xyz}'")
+    conn.commit()
+    return redirect("/showdata")
+
+@myapp.route("/updatedata/<abc>", methods = ["POST"])
+def updateshow(abc):
+    cuser.execute(f"select * from savemydataa where Name = '{abc}'")
+    rec = cuser.fetchone()
+
+    return render_template("updatethisfome.html", data = rec)
+
+
+@myapp.route("/updatehow/<x>", methods = ["POST"])
+def updtehownow(x):
+    if request.method == "POST":
+        name = request.form.get("name")
+        thisemail = request.form.get("email")
+        phnumber = request.form.get("number")
+        msg = request.form.get("msg")
+        cuser.execute(f"update savemydataa set Name = '{name}', email = '{thisemail}', Number =' {phnumber}', message = '{msg}' where Name = '{x}'")
+        conn.commit()
+    return redirect("/showdata")
+
+
 
 if __name__ =="__main__":
     myapp.run(debug = True)
